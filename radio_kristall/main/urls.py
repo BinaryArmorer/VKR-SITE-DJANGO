333b628479987836c99894b6a6eaad33b701870c
BinaryArmorer <NLx64@yandex.ru>
2026-05-11 23:59:38 +0300

    Эксперимент 1

--- radio_kristall/main/urls.py
+++ radio_kristall/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('news/', views.news_list, name='news'),
+    path('vacancies/', views.vacancies_list, name='vacancies'),
+    path('about/', views.about, name='about'),
 ]
--- radio_kristall/main/views.py
+++ radio_kristall/main/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import News, Vacancies
 
-# Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    # Главная страница - показываем последние 3 новости и вакансии
+    latest_news = News.objects.filter(is_published=True)[:3]
+    latest_vacancies = Vacancies.objects.filter(is_published=True)[:3]
+    return render(request, 'main/index.html', {
+        'latest_news': latest_news,
+        'latest_vacancies': latest_vacancies,
+    })
+
+def news_list(request):
+    # Страница всех новостей
+    news = News.objects.filter(is_published=True)
+    return render(request, 'main/news.html', {'news': news})
+
+def vacancies_list(request):
+    # Страница всех вакансий
+    vacancies = Vacancies.objects.filter(is_published=True)
+    return render(request, 'main/vacancies.html', {'vacancies': vacancies})
+
+def about(request):
+    # Страница "О компании"
+    return render(request, 'main/about.html')
