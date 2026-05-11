2df1eafea75cd8074f5dd355ad49ff2ab00fe8ba
BinaryArmorer <NLx64@yandex.ru>
2026-05-11 23:26:02 +0300

    Добавлена поддержка фотографий в базе данных, проведены миграции и настроенна админка

--- radio_kristall/main/admin.py
+++ radio_kristall/main/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import News, Vacancies
 
-# Register your models here.
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publish_date', 'is_published')
+    list_filter = ('is_published', 'publish_date')
+    search_fields = ('title', 'content')
+    readonly_fields = ('news_id',)
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'content', 'image', 'is_published')
+        }),
+        ('Дата публикации', {
+            'fields': ('publish_date',),
+            'classes': ('collapse',)
+        }),
+    )
+
+@admin.register(Vacancies)
+class VacanciesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'salary', 'publish_date', 'is_published')
+    list_filter = ('is_published', 'publish_date')
+    search_fields = ('title', 'description')
+    readonly_fields = ('vacancy_id',)
