c92a7fe8b5f1427f6aac4696cba454dc477881ca
BinaryArmorer <NLx64@yandex.ru>
2026-05-15 02:27:39 +0300

    Перевод на postgresql

--- radio_kristall/radio_kristall/settings.py
+++ radio_kristall/radio_kristall/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'radio_kristall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'radio_kristall',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
