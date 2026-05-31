ce901306b7966f57eca31717d2edec77fc8cf470
BinaryArmorer <NLx64@yandex.ru>
2026-05-31 17:28:59 +0300

    Свел разделы в единый стиль

--- radio_kristall/main/urls.py
+++ radio_kristall/main/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('news/', views.news_list, name='news'),
-    path('vacancies/', views.vacancies_list, name='vacancies'),
     path('about/', views.about, name='about'),
+    path('news/', views.news_list, name='news'),
+    path('news/<int:news_id>/', views.news_detail, name='news_detail'),      # ← новая строка
+    path('vacancies/', views.vacancies_list, name='vacancies'),
+    path('vacancies/<int:vacancy_id>/', views.vacancy_detail, name='vacancy_detail'),  # ← новая строка
     path('catalog/', views.catalog, name='catalog'),
     path('catalog/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- radio_kristall/main/views.py
+++ radio_kristall/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import News, Vacancies, Catalog
 
+
+# Основные страницы
 def index(request):
     return render(request, 'main/index.html')
 
+def about(request):
+    return render(request, 'main/about.html')
+
+
+# Страницы с базой данных
 def news_list(request):
     news = News.objects.filter(is_published=True)
     return render(request, 'main/news.html', {'news': news})
@@ -12,12 +19,19 @@ def vacancies_list(request):
     vacancies = Vacancies.objects.filter(is_published=True)
     return render(request, 'main/vacancies.html', {'vacancies': vacancies})
 
-def about(request):
-    return render(request, 'main/about.html')
-
 def catalog(request):
     products = Catalog.objects.filter(is_published=True)
     return render(request, 'main/catalog.html', {'products': products})
+
+
+# Страницы с подробнотсями для страниц с базой данных
+def news_detail(request, news_id):
+    news_item = get_object_or_404(News, news_id=news_id, is_published=True)
+    return render(request, 'main/news_detail.html', {'news_item': news_item})
+
+def vacancy_detail(request, vacancy_id):
+    vacancy_item = get_object_or_404(Vacancies, vacancy_id=vacancy_id, is_published=True)
+    return render(request, 'main/vacancy_detail.html', {'vacancy_item': vacancy_item})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Catalog, id=product_id, is_published=True)
